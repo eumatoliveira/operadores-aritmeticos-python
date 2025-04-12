4d993c9cb16ff22a8521bcb19a29784af9f7ce64
Matheus Oliveira <112446217+eumatoliveira@users.noreply.github.com>
2025-04-12 09:56:38 -0300

    Add files via upload

---
+++ Operadores aritméticos.py
@@ -0,0 +1,61 @@
+# Operadores aritméticos
+#Operadores aritméticos são usados para realizar operações matemáticas em números.
+#Os operadores aritméticos mais comuns incluem adição (+), subtração (-), multiplicação (*), divisão (/), divisão inteira (//), resto da divisão (%), potência (**), raiz quadrada e raiz cúbica.
+z = 2 * (0 + 2)
+print(z)
+
+# Exemplo de operadores aritméticos em Python
+
+# Adição
+print(1+1)
+
+#Subtração
+print(10-2)
+
+#Multiplicação
+print(4*3)
+
+#Divisão
+print(10/2)
+
+#Divisão inteira
+print(10//3)
+
+#Resto da divisão
+print(10%3)
+
+#Potência
+print(2**3)
+
+#Raiz quadrada
+print(4**(1/2))
+
+#Raiz cúbica
+print(8**(1/3))
+
+produto_1 = 10.50
+produto_2 = 5.50
+
+#Soma dos produtos
+print(produto_1 + produto_2)
+#Subtração dos produtos
+print(produto_1 - produto_2)
+#Multiplicação dos produtos
+print(produto_1 * produto_2)
+#Divisão dos produtos
+print(produto_1 / produto_2)
+#Divisão inteira dos produtos
+print(produto_1 // produto_2)
+#Resto da divisão dos produtos
+print(produto_1 % produto_2)
+#Potência dos produtos
+print(produto_1 ** produto_2)
+#Raiz quadrada dos produtos
+print(produto_1 ** (1/2))
+#Raiz cúbica dos produtos
+print(produto_1 ** (1/3))
+
+x = 10 + 5 * 4
+y= (-2)**3-(-3)**2 * (-5) + (+10)**3 / (+5)**2 - (-4)*(-5)
+print(x)
+print(y)
